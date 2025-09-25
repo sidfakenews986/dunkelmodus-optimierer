278af486112e28e6f0cff02b9039c71c19391871
sidfakenews986 <bisonsicolo@hotmail.com>
2025-09-25 15:36:10 +0100

    Refactor variable name for clarity

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 export function activateDarkMode() {
     const darkModeKey = 'darkMode';
-    const isDarkMode = localStorage.getItem(darkModeKey) === 'true';
+    const isDarkModeActive = localStorage.getItem(darkModeKey) === 'true';
 
     // Toggle dark mode
-    document.body.classList.toggle('dark-mode', !isDarkMode);
-    localStorage.setItem(darkModeKey, !isDarkMode);
+    document.body.classList.toggle('dark-mode', !isDarkModeActive);
+    localStorage.setItem(darkModeKey, !isDarkModeActive);
 }
 
 // Activate dark mode on page load if saved
